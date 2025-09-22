a760e1c6a00391a47593be17ba79d248f2c44f50
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-22 09:42:58 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,5 @@
-// Empty (app-level uses its own)
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
+}
