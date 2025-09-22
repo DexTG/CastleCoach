ed7ef241332bc2be190834a9735c83eb13560c89
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-22 11:04:02 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,4 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositories { google(); mavenCentral() } }
+rootProject.name = "CastleCoach"
+include(":app")
