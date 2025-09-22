aac47c95961d094156e56c7e7db11a80778c32bb
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-22 10:10:02 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,11 @@ implementation(composeBom)
 androidTestImplementation(composeBom)
 
 
+implementation("androidx.room:room-runtime:2.6.1")
+implementation("androidx.room:room-ktx:2.6.1")
+kapt("androidx.room:room-compiler:2.6.1")
+
+
 implementation("androidx.core:core-ktx:1.13.1")
 implementation("androidx.activity:activity-compose:1.9.2")
 implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
