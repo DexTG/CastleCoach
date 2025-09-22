1696119aa0b4554ee66f508977f4b65124b1b287
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-22 10:34:54 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ kapt("androidx.room:room-compiler:$room") // if using KAPT
 // Java Time backport for older devices (optional if minSdk >= 26)
 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 implementation("androidx.health.connect:connect-client:1.1.0-alpha10")
-implementation("androidx.health.connect:connect-client:1.1.0-alpha10")
+
 val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
 implementation(composeBom)
 androidTestImplementation(composeBom)
