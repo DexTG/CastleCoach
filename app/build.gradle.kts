95371d6cfc38859d7cbefbdc205969b2429165c0
DexTG <86502511+DexTG@users.noreply.github.com>
2025-09-22 08:42:00 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ debugImplementation("androidx.compose.ui:ui-tooling")
 
 implementation("androidx.work:work-runtime-ktx:2.9.1")
 
+implementation("com.google.android.material:material:1.12.0")
 
 // Preferences
 implementation("androidx.datastore:datastore-preferences:1.1.1")
